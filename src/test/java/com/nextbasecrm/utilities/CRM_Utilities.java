109a3ec6c0c538470bc8c0ab557cada4671a499a
Mihajlo Stamenkovic <mihajlonish@hotmail.com>
2022-02-22 16:37:42 -0500

    CRM Log_in utility

---
+++ src/test/java/com/nextbasecrm/utilities/CRM_Utilities.java
@@ -0,0 +1,21 @@
+package com.nextbasecrm.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CRM_Utilities {
+
+
+    public static void crm_login(WebDriver driver, String username, String password){
+        //3. Enter valid username
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys(username);
+        //4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys(password);
+        //5. Click to Log In button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@class='login-btn']"));
+        loginButton.click();
+    }
+}
