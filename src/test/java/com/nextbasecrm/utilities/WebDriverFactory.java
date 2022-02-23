376e2bd8929efaa143f6b1be18768bae94c07023
Mihajlo Stamenkovic <mihajlonish@hotmail.com>
2022-02-22 19:20:58 -0500

    WebDriverFactory utility

---
+++ src/test/java/com/nextbasecrm/utilities/WebDriverFactory.java
@@ -0,0 +1,22 @@
+package com.nextbasecrm.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    public  static WebDriver getDriver(String browserType) {
+        if (browserType.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+
+        }else {
+            System.out.println("There is no such driver");
+            return null;
+        }
+    }
+}
