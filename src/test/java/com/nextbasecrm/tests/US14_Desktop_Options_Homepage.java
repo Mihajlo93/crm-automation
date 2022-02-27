3375dcfc6223271fd5b63a95f5746a14c23fe27c
Mihajlo Stamenkovic <mihajlonish@hotmail.com>
2022-02-27 12:46:50 -0500

    Update

--- src/test/java/com/nextbasecrm/tests/US14_Desktop_Options_Homepage.java
+++ src/test/java/com/nextbasecrm/tests/US14_Desktop_Options_Homepage.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class Desktop_Options_Homepage {
+public class US14_Desktop_Options_Homepage {
 
     WebDriver driver;
 
